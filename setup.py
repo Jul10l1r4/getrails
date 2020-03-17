0328c20317e5817eeda372727fa0750c48c914cb
jul10l1r4 <jul10l1r4@disroot.org>
2020-03-17 08:05:57 -0300

    :unicorn: Fix url link

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='getrails', 
+    name='getrails',
     version=1.5,
     author="Julio Lira",
     author_email="jul10l1r4@disroot.org",
     description="Tool of OSINT and Dork hacking that work with Google and Duckduckgo",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Vault-Cyber-Security/getrails",
+    url="https://github.com/Vault-Cyber-Security/getrails/tree/lib",
     packages=["getrails", "getrails.google", "getrails.duckduckgo"],
     classifiers=[
         "Programming Language :: Python :: 3",
