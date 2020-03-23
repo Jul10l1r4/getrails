64d3120490bc13444becc2b593d8ff00442b6dda
Julio Lira <jul10l1r4@disroot.org>
2020-03-23 14:18:01 -0300

    :boom: new version

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='getrails',
-    version=2.1,
+    version=2.2,
     author="Julio Lira",
     author_email="jul10l1r4@disroot.org",
     description="Tool of OSINT and Dork hacking that work with Google and Duckduckgo",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Jul10l1r4/getrails",
-    packages=["getrails", "getrails.google", "getrails.duckduckgo"],
+    packages=["getrails", "getrails.google", "getrails.duckduckgo", "getrails.torch"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
