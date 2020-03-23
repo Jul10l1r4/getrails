adf1c1cb6f399d6323070d69564b4bc45643c6da
Julio Lira <jul10l1r4@disroot.org>
2020-03-23 20:51:07 -0300

    :unicorn: update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='getrails',
-    version=2.5,
+    version=2.6,
     author="Julio Lira",
     author_email="jul10l1r4@disroot.org",
     description="Tool of OSINT and Dork hacking that work with Google and Duckduckgo",
