fccee8985293351dcb3644c8d887c7ca79d35b09
Julio Lira <jul10l1r4@disroot.org>
2020-03-23 17:53:48 -0300

    :bug: Fix PEP 257 shebang and bare-except W0702

--- getrails/__init__.py
+++ getrails/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from getrails.duckduckgo.search import go_duck
 from getrails.google.search import go_gle
 from getrails.torch.search import go_onion
@@ -6,7 +5,7 @@ from getrails.torch.search import go_onion
 def search (query):
     try:
         result = go_gle(query)
-    except:
+    except urllib.error.HTTPError:
         result = go_duck(query)
 
     result = result.append(go_onion(query))
