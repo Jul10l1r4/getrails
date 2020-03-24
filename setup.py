d55cfe8b6ea58932d01e904b5af163908e90ca98
Julio Lira <julio.lira@owasp.org>
2020-03-24 14:58:17 -0300

    Update version and add new classifieds

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='getrails',
-    version=2.7,
+    version=2.8,
     author="Julio Lira",
     author_email="jul10l1r4@disroot.org",
     description="Tool of OSINT and Dork hacking that work with Google and Duckduckgo",
@@ -13,9 +13,17 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Jul10l1r4/getrails",
     packages=["getrails", "getrails.google", "getrails.duckduckgo", "getrails.torch"],
+    license="GPLv3",
+    keywords="getrails get trails google duckduckgo tor onion torch osint",
+    install_requires=[
+          'mechanicalsoup',
+          'google',
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
+        "Topic :: Security",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
         ],
     )
