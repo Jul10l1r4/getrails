1f7dfb24850eab0e5e4ec4e873b0bb6bd1c932c2
Julio Lira <jul10l1r4@disroot.org>
2020-03-23 21:34:25 -0300

    :tada: Fix except error

--- getrails/__init__.py
+++ getrails/__init__.py
@@ -1,11 +1,12 @@
 from getrails.duckduckgo.search import go_duck
 from getrails.google.search import go_gle
 from getrails.torch.search import go_onion
+from urllib import error
 
 def search (query):
     try:
         result = go_gle(query)
-    except urllib.error.HTTPError:
+    except error.HTTPError:
         result = go_duck(query)
 
     result = result.append(go_onion(query))
